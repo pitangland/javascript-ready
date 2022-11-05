aa35dedea4ed8e28c978a5c6c5ee63730bd59d0e
pitang <wpeffect@gmail.com>
2022-11-05 19:37:47 +0900

    11/5 Forms

--- pitang-project-1/src/index.js
+++ pitang-project-1/src/index.js
@@ -12,6 +12,7 @@ import Accommodate from './chapter_07/Accommodate';
 import ConfirmButton from './chapter_08/ComfirmButton';
 import LandingPage from './chapter_09/LandingPage';
 import AttendanceBook from './chapter_10/AttendanceBook';
+import SignUp from './chapter_11/SignUp';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -20,7 +21,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render (
   <React.StrictMode>
-    <AttendanceBook />
+    <SignUp />
   </React.StrictMode>,
   document.getElementById('root')
 );

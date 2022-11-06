b322b27fee1ee6a6e33292db0caa08a85c54b33e
pitang <wpeffect@gmail.com>
2022-11-06 20:19:24 +0900

    11/6 Composition, Inheritance

---
+++ pitang-project-1/src/chapter_13/ProfileCard.jsx
@@ -0,0 +1,12 @@
+import Card from "./Card";
+
+function ProfileCard(props) {
+    return (
+        <Card title = "pitang" backgroundColor="#4ea04e">
+            <p>안녕하세요! 피탕입니다!</p>
+            <p>리액트를 사용해서 개발중입니다~</p>
+        </Card>
+    );
+}
+
+export default ProfileCard;

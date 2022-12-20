7928773d301291bdbcb05dd579e72e334f3b82e9
pitang <wpeffect@gmail.com>
2022-12-20 20:48:56 +0900

    글씨 양옆으로 선 그리기 실패

--- pitang-project-1/src/icon-practice/icon-practice.js
+++ pitang-project-1/src/icon-practice/icon-practice.js
@@ -6,22 +6,58 @@ import styled from "styled-components";
 // react-icons 들어가면 여러 사이트에 있는 아이콘을 사용할 수 있음.
 // font Awesome으로 사용해보자!
 import { BsDownload } from "react-icons/bs";
-// <GrDownload/> 로 사용하면된다!
-
 import { IoReloadOutline } from "react-icons/io5";
+
+// Line은 이 아이콘 방법말고 다른 방법으로 해봐야겠다.
+// import { AiOutlineLine } from "react-icons/ai";
 
 const Download = styled(BsDownload)``;
 const Reload = styled(IoReloadOutline)`
   color: #da234f;
 `;
 
+// const Line = styled(AiOutlineLine)`
+//   flex-direction: row;
+//   width: 56px;
+// `;
+
+// 이게 대체 왜 안되는지 모르겠네 휴
+const Line = styled.div`
+  display: flex;
+  flex-basis:100%;
+  align-items: center;
+  color: black;
+  font-size: 14px;
+  margin: 8px 0px;
+
+  &::before {
+    content: "";
+    flex-grow: 1,
+    margin: 0px 16px;
+    background: black;
+    height: 1px;
+    font-size: 0px;
+    line-height: 0px;
+  }
+
+  &::after {
+    content: "";
+    flex-grow: 1,
+    margin: 0px 16px;
+    background: black;
+    height: 1px;
+    font-size: 0px;
+    line-height: 0px;
+  }
+`;
+
 const Wrapper = styled.div`
   padding: 1rem;
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
   align-items: flex-start;
   justify-content: flex-start;
-  background-color: lightgrey;
+  // background-color: lightgrey;
 `;
 
 const Block = styled.div`
@@ -63,8 +99,11 @@ function Blocks(props) {
             backgroundColor={blockItem.backgroundColor}
           >
             {blockItem.label}
+            {/* 다운로드 아이콘 생성 */}
             <Download />
+            {/* 다시 다운로드 아이콘 생성 */}
             <Reload />
+            <Line>공유하기 왜 안돼?</Line>
           </Block>
         );
       })}

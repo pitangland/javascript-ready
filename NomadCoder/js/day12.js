e3e1831fb24e1f32b8e093878e5b9fd6c024aca9
pitang <wpeffect@gmail.com>
2023-01-08 18:02:03 +0900

    nomadCoder day12 make toDoList

--- NomadCoder/js/day12.js
+++ NomadCoder/js/day12.js
@@ -1,1 +1,61 @@
-//todolist
+// 7.0 todolist
+const toDoForm = document.getElementById("todo-form");
+// const toDoInput = toDoForm.querySelector("input");
+const toDoInput = document.querySelector("#todo-form input");
+const toDoList = document.getElementById("todo-list");
+
+const TODOS_KEY = "todos";
+
+let toDos = [];
+
+function saveToDos() {
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+}
+
+function deleteToDo(event) {
+  // 어디서 클릭됐는지 알 수 있음.
+  //   console.log(event.target.parentElement.innerText);
+  // 우리가 삭제해야 할 li
+  const li = event.target.parentElement;
+  li.remove();
+  toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
+  saveToDos();
+}
+
+function paintToDo(newTodo) {
+  const li = document.createElement("li");
+  li.id = newTodo.id;
+  const span = document.createElement("span");
+  span.innerText = newTodo.text;
+  const button = document.createElement("button");
+  button.innerText = "X";
+  button.addEventListener("click", deleteToDo);
+  li.appendChild(span);
+  li.appendChild(button);
+  toDoList.appendChild(li);
+}
+
+function handleToDoSubmit(event) {
+  event.preventDefault();
+  const newTodo = toDoInput.value;
+  toDoInput.value = "";
+
+  const newTodoObj = {
+    text: newTodo,
+    id: Date.now(),
+  };
+
+  toDos.push(newTodoObj);
+  paintToDo(newTodoObj);
+  saveToDos();
+}
+
+toDoForm.addEventListener("submit", handleToDoSubmit);
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if (savedToDos !== null) {
+  const parsedTodos = JSON.parse(savedToDos);
+  toDos = parsedTodos;
+  parsedTodos.forEach(paintToDo);
+}

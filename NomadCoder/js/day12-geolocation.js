05b6054f7ffbed38069d3a93e31ec1a130b2b38f
pitang <wpeffect@gmail.com>
2023-01-08 18:02:22 +0900

    nomadCoder make geolocation

---
+++ NomadCoder/js/day12-geolocation.js
@@ -0,0 +1,26 @@
+const API_KEY = "86c9b859e02faaa5680f26a34785d6cc";
+const weather = document.querySelector("#weather span:first-child");
+const city = document.querySelector("#weather span:last-child");
+
+function onGeoOk(position) {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+
+  // promise는 당장 뭔가 일어나지 않고 시간이 좀 걸린 뒤에 일어나는 것.
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+
+      console.log(data);
+    });
+}
+
+function onGeoError() {
+  alert("Can't find you. No weather for you");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
